a6071262bed43390ab749e02fc7ef779cb2357e6
Eptarch <68643161+Eptarch@users.noreply.github.com>
2022-02-23 21:47:27 +0500

    🎨 Update two-fer (#5)

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
 
-def two_fer(name: Optional[str] = 'you') -> str:
-    return f'One for {name}, one for me.'
+def two_fer(name: Optional[str] = None) -> str:
+    return f"One for {name or 'you'}, one for me."
+
