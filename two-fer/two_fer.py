dda50bcd8e63d71b53ab2f726123d4545dede7ad
BadHatStrong <notsobadlife@gmail.com>
2022-02-23 16:12:13 +0400

    Two-fer solution

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,3 +1,5 @@
-def two_fer(name='you'):
-    if name == name:        
-        return "One for " + name + ", one for me."
+from typing import Optional
+
+
+def two_fer(name: Optional[str] = 'you') -> str:
+    return f'One for {name}, one for me.'
