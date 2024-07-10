432e64f0fb66d91165cd712ee3c22172929358f3
badhatstrong <badhatstrong@gmail.com>
2024-07-10 08:29:12 +0000

    Darts solution

--- darts/darts.py
+++ darts/darts.py
@@ -1,12 +1,11 @@
-def score(x, y):
+def score(x: int, y:int) -> int:
     result = 0
-    if pow(x, 2) + pow(y, 2) <= 1:
+    circle_checkout: int = x ** 2 + y ** 2
+    if circle_checkout <= 1:
         result = 10
-    elif pow(x, 2) + pow(y, 2) <= 25:
+    elif circle_checkout <= 25:
         result = 5
-    elif pow(x, 2) + pow(y, 2) <= 100:
+    elif circle_checkout <= 100:
         result = 1
-    else:
-        result = 0
     return result
 
